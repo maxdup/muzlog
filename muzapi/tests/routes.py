23cc4a99b39414c58f30272b2f13569f3db363d7
maxdup <mdupuis@hotmail.ca>
2018-12-03 23:11:45 -0500

    test cms page

--- muzapi/tests/routes.py
+++ muzapi/tests/routes.py
@@ -1,8 +1,8 @@
 from muzapi import db, User
 import unittest
-
 from muzapi.util_fakedata import fake_user, fake_album
 from muzapi.models import User, Role
+from muzapi.res_roles import ensure_roles
 from muzapi import create_app
 
 
@@ -12,9 +12,15 @@ class RoutesTestCase(unittest.TestCase):
         app = create_app('config_test')
 
         with app.app_context():
+            db.connection.drop_database(app.config['MONGODB_DB'])
+            ensure_roles()
+
+            self.r_logger = Role.objects.get(name="logger")
+
             self.client = app.test_client()
 
             self.user = fake_user()
+            self.user.roles.append(self.r_logger)
             self.user.save()
             self.user.reload()
 
@@ -41,4 +47,16 @@ class RoutesTestCase(unittest.TestCase):
             sess['_fresh'] = True
 
         r = self.client.get('album/' + str(self.album.id))
+        self.assertEqual(r.status_code, 200)
+
+    def test_get_cms_page(self):
+
+        r = self.client.get('cms/' + str(self.album.id))
+        self.assertEqual(r.status_code, 302)
+
+        with self.client.session_transaction() as sess:
+            sess['user_id'] = str(self.user.id)
+            sess['_fresh'] = True
+
+        r = self.client.get('cms/' + str(self.album.id))
         self.assertEqual(r.status_code, 200)
